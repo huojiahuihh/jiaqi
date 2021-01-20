3fc462ded71b9bef7369981b00a697b4982ba9aa
huojiahuihh <huojiahuihh@163.com>
2021-01-20 16:49:18 +0800

    分类增删改

--- mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryService.java
+++ mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryService.java
@@ -2,11 +2,14 @@ package com.baidu.shop.service;
 
 import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
+import com.google.gson.JsonObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Api(tags = "商品分类接口")
@@ -14,7 +17,23 @@ import java.util.List;
 public interface CategoryService {
 
     //查询
-    @ApiOperation(value = "查询商品分类")
+    @ApiOperation(value = "通过查询商品分类")
     @GetMapping(value = "category/list")
-    Result<List<CategoryEntity>> getCategoryByPid(Integer pid);
+    Result<List<CategoryEntity>> getCategoryByPid(@NotNull Integer pid);
+
+    //删除
+    @ApiOperation(value ="通过id删除分类")
+    @DeleteMapping(value = "category/del")
+    Result<JsonObject> delCategory(@NotNull Integer id);
+
+    //修改
+    @ApiOperation(value = "修改分类")
+    @PutMapping(value = "category/edit")
+    Result<JsonObject> editCategory(@Validated({MingruiOperation.Update.class})@RequestBody CategoryEntity categoryEntity);
+
+    //新增
+    @ApiOperation(value = "新增分类")
+    @PostMapping(value = "category/save")
+    Result<JsonObject> saveCategory(@Validated({MingruiOperation.Add.class}) @RequestBody CategoryEntity categoryEntity);
+
 }
--- mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -5,7 +5,11 @@ import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.CategoryService;
+import com.baidu.shop.utils.ObjectUtil;
+import com.google.gson.JsonObject;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 
 import javax.annotation.Resource;
@@ -23,5 +27,56 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
         categoryEntity.setParentId(pid);
         List<CategoryEntity> list = categoryMapper.select(categoryEntity);
         return this.setResultSuccess(list);
+    }
+
+    @Override
+    @Transactional
+    public Result<JsonObject> delCategory(Integer id) {
+        //检验一下Id是否合法  id不能为空并且不能是负数
+        if(ObjectUtil.isNull(id) || id <= 0) return this.setResultError("id不合法");
+
+        //通过Id查询数据
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+
+        //判断当前节点是否存在
+        if(ObjectUtil.isNull(categoryEntity)) return this.setResultError("数据不存在");
+
+        //判断是否为父节点
+        if (categoryEntity.getIsParent() == 1) return this.setResultError("当前为父节点 不能删除");
+
+        //通过当前父节点id查询 当前父节点下是否还有其他子节点
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+
+        //根据删除的parentId为节点 查询父级节点的状态
+        if (list.size() <=1){
+            CategoryEntity UpdateCategoryEntity = new CategoryEntity();
+            UpdateCategoryEntity.setParentId(0);
+            UpdateCategoryEntity.setId(categoryEntity.getParentId());
+
+            categoryMapper.updateByPrimaryKey(UpdateCategoryEntity);
+        }
+        categoryMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
+    }
+
+    @Override
+    @Transactional
+    public Result<JsonObject> editCategory(CategoryEntity categoryEntity) {
+        categoryMapper.updateByPrimaryKeySelective(categoryEntity);
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JsonObject> saveCategory(CategoryEntity categoryEntity) {
+        //新增一个节点的时候 在新增的节点下再新增一个节点的时候把节点改为父节点
+        CategoryEntity parentCategoryEntity = new CategoryEntity();
+        parentCategoryEntity.setId(categoryEntity.getParentId());
+        parentCategoryEntity.setIsParent(1);
+        categoryMapper.updateByPrimaryKeySelective(parentCategoryEntity);
+
+        categoryMapper.insertSelective(categoryEntity);
+        return this.setResultSuccess();
     }
 }
